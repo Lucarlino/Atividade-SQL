604776f8ee786727af669f6976170ebb1ec47751
Lucas Souza Santos <00001118720532sp@aluno.educacao.sp.gov.br>
2025-09-24 18:12:53 -0300

    atualizando mySQL

--- festa.sql
+++ festa.sql
@@ -12,7 +12,14 @@ VALUES ('Lucas', 'Lucas@gmail.com'),('Carlos', 'Carlos@gmail.com');
 
 select * from Cliente;
 
-select * from Cliente where idCliente = 2;
+select * from Cliente where idCliente = 1;
+
+update Cliente set NomeCliente = "Nilson", 
+				   EmailCliente="Nilson@gmail.com" 	
+				   where idCliente = 1;
+                   
+delete from Cliente where idCliente = 1;
+
 
 create table Endereco(
     idEnd int auto_increment primary key,
@@ -20,6 +27,19 @@ create table Endereco(
     CEP varchar (15) not null,
     Bairro varchar (30)
 );
+
+Insert into Endereco (Logradouro, Bairro, CEP)
+values("Rua 15", "Centro", "058751-120");
+
+select * from Endereco;
+select Logradouro, Bairro from Endereco where idEnd = 1; 
+select * from Endereco where idEnd = 1;
+
+update Endereco set Logradouro ="AV.Brasil",
+					Bairro = "Paulista", CEP = "98765-432"
+                    where idEnd = 1;
+
+delete from Endereco where idEnd = 2;
 
 create table residencia (
     idRes int auto_increment primary key,
@@ -29,6 +49,22 @@ create table residencia (
     foreign key (idCliente) references cliente(idCliente),
     foreign key (idEnd) references Endereco (idEnd)
 );
+
+insert into residencia (idCliente, idEnd, NomeRes)
+			values(2, 1,'APTO 45, Bloco B');
+select * from residencia;
+
+select
+t1.idRes,
+t2.NomeCliente,
+t3.Logradouro,
+t3.Bairro,
+t3.CEP,
+t1.NomeRes as Complemento, 
+t2.EmailCliente
+from residencia as t1
+inner join Cliente as t2 on t1.idCliente = t2.idCliente
+inner join Endereco as t3 on t1.idEnd = t3.idEnd;
 
 create table Tema (
 	idTema int primary key auto_increment,
