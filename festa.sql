285ab84900d37bbc319b5f06b132d52d7e0194e7
Lucas Souza Santos <00001118720532sp@aluno.educacao.sp.gov.br>
2025-09-24 16:18:01 -0300

    atualizando FestaSQL

--- festa.sql
+++ festa.sql
@@ -1,15 +1,22 @@
-create database festa;
-use festa;
+drop database if exists festa1;
+create database festa1;
+use festa1;
 
-create table cliente(
+CREATE TABLE cliente(
     idCliente int auto_increment primary key,
     NomeCliente varchar(50) not null,
-    EmailCliente varchar(30) not null,
+    EmailCliente varchar(30) not null
 );
+INSERT INTO Cliente (NomeCliente, EmailCliente)
+VALUES ('Lucas', 'Lucas@gmail.com'),('Carlos', 'Carlos@gmail.com');
 
-create table endereco(
-    idEnd int auto_increment primary key;
-    Lograndouro varchar (30) not null,
+select * from Cliente;
+
+select * from Cliente where idCliente = 2;
+
+create table Endereco(
+    idEnd int auto_increment primary key,
+    Logradouro varchar (30) not null,
     CEP varchar (15) not null,
     Bairro varchar (30)
 );
@@ -19,6 +26,27 @@ create table residencia (
     idCliente int not null,
     idEnd int not null,
     NomeRes varchar(50) not null, 
-    foreign key (idCliente) reference (idCliente)
-    foreign key (idEnd) reference (idEnd)
+    foreign key (idCliente) references cliente(idCliente),
+    foreign key (idEnd) references Endereco (idEnd)
+);
+
+create table Tema (
+	idTema int primary key auto_increment,
+    NomeTema varchar (50) not null
+);
+
+create table Itens_Tema(
+	idItem int primary key auto_increment,
+    idTema int not null,
+    NomeItem varchar(30) not null,
+    foreign key (idTema) references Tema (idTema)
+);
+
+create table Festa(
+	idFesta int primary key auto_increment,
+    idCliente int not null,
+    idTema int not null, 
+    idEnd int not null, 
+    dtFesta date not null,
+    valorFesta decimal(10,2) not null
 );
