4a12e61e486bbff402266f37d1e2e06ecbf38829
Lucas Souza Santos <00001118720532sp@aluno.educacao.sp.gov.br>
2025-09-26 18:02:47 -0300

    atualizando atividade

--- festa.sql
+++ festa.sql
@@ -8,7 +8,9 @@ CREATE TABLE cliente(
     EmailCliente varchar(30) not null
 );
 INSERT INTO Cliente (NomeCliente, EmailCliente)
-VALUES ('Lucas', 'Lucas@gmail.com'),('Carlos', 'Carlos@gmail.com');
+VALUES ('Lucas', 'Lucas@gmail.com'),('Carlos', 'Carlos@gmail.com') ,
+('Bia', 'Bia@gmail.com'), ('Augusto', 'Augusto@gamil.com'),
+('Eric', 'Eric@gmail.com'), ('Guilherme', 'Guilherme@gmail.com');
 
 select * from Cliente;
 
@@ -18,7 +20,7 @@ update Cliente set NomeCliente = "Nilson",
 				   EmailCliente="Nilson@gmail.com" 	
 				   where idCliente = 1;
                    
-delete from Cliente where idCliente = 1;
+
 
 
 create table Endereco(
@@ -29,7 +31,8 @@ create table Endereco(
 );
 
 Insert into Endereco (Logradouro, Bairro, CEP)
-values("Rua 15", "Centro", "058751-120");
+values("Rua 78", "Morro Bento", "058751-145"),("Rua 50", "São Bento", "058751-120"),
+("Rua 15", "Anhanguera", "058751-220"), ("Rua 05", "Centro", "058751-120");
 
 select * from Endereco;
 select Logradouro, Bairro from Endereco where idEnd = 1; 
@@ -51,8 +54,12 @@ create table residencia (
 );
 
 insert into residencia (idCliente, idEnd, NomeRes)
-			values(2, 1,'APTO 45, Bloco B');
+			values(1, 1,'Casa de Barro'),
+            (2, 2,'Casa de Vidro'),
+            (3, 3,'APTO 85, Torre 1');
+
 select * from residencia;
+select * from Endereco;
 
 select
 t1.idRes,
@@ -66,11 +73,42 @@ from residencia as t1
 inner join Cliente as t2 on t1.idCliente = t2.idCliente
 inner join Endereco as t3 on t1.idEnd = t3.idEnd;
 
+select
+t1.idRes,
+t2.NomeCliente,
+t3.Logradouro,
+t3.Bairro,
+t3.CEP,
+t1.NomeRes as Complemento, 
+t2.EmailCliente
+from residencia as t1
+inner join Cliente as t2 on t1.idCliente = t2.idCliente
+inner join Endereco as t3 on t1.idEnd = t3.idEnd 
+where t2.idCliente = 3;
+
+update residencia set idCliente = 5,
+					  idEnd = 3,
+                      NomeRes = 'Casa III' where idRes = 1;
+                      
+select * from residencia;
+                      
 create table Tema (
 	idTema int primary key auto_increment,
     NomeTema varchar (50) not null
 );
 
+insert into Tema(NomeTema)
+			values ('Naruto'), ('Dragon Ball'), ('Homem Aranha'),
+				   ('Batman'), ('One Piece'); 
+                   
+select * from Tema;
+
+select * from Tema where idTema = 1;
+
+update Tema set NomeTema = 'Boruto' where idTema = 2;
+
+delete from Tema where idTema = 2;
+                   
 create table Itens_Tema(
 	idItem int primary key auto_increment,
     idTema int not null,
@@ -78,6 +116,25 @@ create table Itens_Tema(
     foreign key (idTema) references Tema (idTema)
 );
 
+insert into Itens_Tema(idTema, NomeItem)
+				values(1,'Sharingan'), (1,'Byakugan'), 
+                (1,'Kunai'), (1,'Rinnegan'), (1,'Jiraya'),
+                (3,'Teia'), (3,'Mary Jane'), (3,'Duende Verde'),(3,'Venom'),
+                (4,'Robin'), (4,'BatCaverna'), (4,'Batman'), (4,'Coringa');
+                
+select * from Itens_Tema;
+
+select * from Itens_Tema as t1
+inner join Tema as t2 on t1.idTema = t2.idTema;
+
+select
+t1.idItem, t2.NomeTema, t1.NomeItem 
+from Itens_Tema as t1
+inner join Tema as t2 on t1.idTema = t2.idTema;
+
+/*Fazer sozinho*/
+-- UPDATE
+-- DELETE
 create table Festa(
 	idFesta int primary key auto_increment,
     idCliente int not null,
@@ -86,3 +143,8 @@ create table Festa(
     dtFesta date not null,
     valorFesta decimal(10,2) not null
 );
+
+-- INSERT
+-- SELECT com INNER JOIN
+-- UPDATE
+-- DELETE
